e56efbabb12d745d84bc2f9d517ef34acb4e24a4
Otto Tarkka <tarkkaot@puhti-login1.bullx>
2022-09-22 11:21:02 +0300

    Fixed critical bugs

--- classifier-with-comet.py
+++ classifier-with-comet.py
@@ -1,31 +1,12 @@
-#this is a script for running the classifier
-#based largely on https://github.com/TurkuNLP/Deep_Learning_in_LangTech_course/blob/master/hf_trainer_bert.ipynb
+# This is a script for running the classifier
+# Based largely on https://github.com/TurkuNLP/Deep_Learning_in_LangTech_course/blob/master/hf_trainer_bert.ipynb
 
 
-import argparse
-
-parser = argparse.ArgumentParser(
-            description="A script for predicting the political affiliation of the politician who spoke a given speech in Finnish parliament"
-        )
-parser.add_argument('--xp_name', required=True,
-    help = 'give a name for this experiment')
-parser.add_argument('--comet_key', required=True,
-    help = 'give your comet api key'
-parser.add_argument('--comet_workspace', required=True,
-    help="give the name of your comet workspace")
-parser.add_argument('comet_project', required=True,
-    help="give the name of you comet project")
-parser.add_argument('--learning_rate', type=float, default=0.00001,
-    help="set trainer learning rate")
-args = parser.parse_args()
-
-
-# import comet_ml for logging and plotting
-#comet_ml must be imported before any other ML framework for it to work properly
-from comet_ml import Experiment, Optimizer
+# Import comet_ml for logging and plotting
+# comet_ml must be imported before any other ML framework for it to work properly
+from comet_ml import Experiment
 import comet_ml
 
-#import rest of required modules
 from pprint import PrettyPrinter
 pprint = PrettyPrinter(compact=True).pprint
 from datasets import load_dataset
@@ -34,6 +15,22 @@ import transformers
 import pandas as pd
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 import os
+import argparse
+
+parser = argparse.ArgumentParser(
+            description="A script for predicting the political affiliation of the politician who spoke a given speech in Finnish parliament"
+        )
+parser.add_argument('--xp_name', required=True,
+    help = 'give a name for this experiment')
+parser.add_argument('--comet_key', required=True,
+    help = 'give your comet api key')
+parser.add_argument('--comet_workspace', required=True,
+    help="give the name of your comet workspace")
+parser.add_argument('--comet_project', required=True,
+    help="give the name of you comet project")
+parser.add_argument('--learning_rate', type=float, default=0.00001,
+    help="set trainer learning rate")
+args = parser.parse_args()
     
 def main():    
     
@@ -46,35 +43,35 @@ def main():
         )
         return experiment
     
-    experiment = comet(api_key = args.comet_key, args.comet_workspace, args.comet_project)
+    experiment = comet(args.comet_key, args.comet_workspace, args.comet_project)
     
-    #setup data paths
+    # Setup data paths
     train_data = '../data/parl_speeches_2000-2021_threeparties_train.csv'
     validation_data = '../data/parl_speeches_2000-2021_threeparties_validation.csv'
     test_data = '../data/parl_speeches_2000-2021_threeparties_test.csv'
     
-    cache_dir = '../hf_cache' #huggingface cache can get bloated with multiple runs so save to disk with enough storage
-    output_dir = f'../results/models/model-with-comet/{args.xp_name}' #where results are saved
+    cache_dir = '../hf_cache' # hf cache can get bloated with multiple runs so save to disk with enough storage
+    output_dir = f'../results/models/model-with-comet/{args.xp_name}' # Where results are saved
     
-    #load in train and test data
+    # Load in train and test data
     print('Loading dataset...')
-    #these datasets contain speeches between 2000 and 2001 from the three parties that held the bulk of speehces: KOK, SD, KESK
-    #the data has been shuffled, i.e., its not in chronological order
-    dataset = load_dataset('csv', data_files = {'train': 'train_data',
-                                                'validation' : 'validation_data',
-                                                'test': 'test_data'},
+    # Speeches between 2000 and 2001 from the three parties that held the bulk of speehces: KOK, SD, KESK
+    # The data has been shuffled, i.e., its not in chronological order
+    dataset = load_dataset('csv', data_files = {'train': train_data,
+                                                'validation' : validation_data,
+                                                'test': test_data},
                                                 cache_dir = cache_dir)
     
-    #shuffle the dataset for good measure
+    # Shuffle the dataset for good measure
     dataset=dataset.shuffle()
     
-    #let's see what the data looks like
+    # Let's see what the data looks like
     print("Here's the dataset:")
     pprint(dataset)
     print('This is an example sentence from the dataset:')
     pprint(dataset["train"][0])
     
-    #get the number of labels, which is required for the model
+    # Get the number of labels, which is required for the model
     def get_labels(dataset):
         train_labels = dataset['train']['label']
         label_ints = sorted(list(set(train_labels)))
@@ -91,12 +88,12 @@ def main():
     
     num_labels, id2label_in_data = get_labels(dataset)
     
-    #initialise model and tokenizer
+    # Initialise model and tokenizer
     model_name='TurkuNLP/bert-base-finnish-cased-v1'
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
     
     # Define a simple function that applies the tokenizer
-    # maximum length of BERT models is 512 due to the position embeddings
+    # Maximum length of BERT models is 512 due to the position embeddings
     def tokenize(example):
         return tokenizer(
             example["text"],
@@ -105,11 +102,11 @@ def main():
         )
         
     # Apply the tokenizer to the whole dataset using .map()
-    #batched means that tokenization is done in batched, and hence, faster
+    # Batched means that tokenization is done in batches, and hence, faster
     print('Tokenizing...')
     dataset = dataset.map(tokenize, batched=True)
     
-    #print test sample of tokens
+    # Print test sample of tokens
     example = dataset['train'][0]['text']
     print('This is an example sentence tokenized:')
     tokenized = tokenizer(example)
@@ -118,8 +115,10 @@ def main():
     tokens = tokenizer.tokenize(example)
     print(tokens)
     
-    #initialise model for sequence classification
-    model = transformers.AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=num_labels, id2label = id2label_in_data)
+    # Iinitialise model for sequence classification
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(model_name, 
+                                                                            num_labels=num_labels, 
+                                                                            id2label = id2label_in_data)
     
     # Set training arguments
     trainer_args = transformers.TrainingArguments(
@@ -140,7 +139,6 @@ def main():
     #function for computing accuracy and F score
     def get_example(index):
         return dataset['validation'][index]["text"]
-    
     
     def compute_metrics(pred):
         experiment = comet_ml.get_global_experiment()
@@ -165,22 +163,22 @@ def main():
     
         return {"accuracy": acc, "f1": f1, "precision": precision, "recall": recall}
     
-    #data collator pads the input to be of uniform size
+    # Data collator pads the input to be of uniform size
     data_collator = transformers.DataCollatorWithPadding(tokenizer)
     
     # Argument gives the number of steps of patience before early stopping
     early_stopping = transformers.EarlyStoppingCallback(
-        early_stopping_patience=5)
+        early_stopping_patience=10)
     
-    # print a sample of test and validation labels to see that they are not ordered
+    # Print a sample of test and validation labels to see that they are not ordered
     print('Sample of test and validation labels:')
     print(dataset["test"]["label"][:20])
     print(dataset["validation"]["label"][:20])
     
     
-    #train model
+    # Train model
     
-    #comet_ml automatically logs training data
+    # comet_ml automatically logs training data
     os.environ["COMET_LOG_ASSETS"] = "True"
     
     trainer = None
@@ -197,7 +195,9 @@ def main():
     
     trainer.train()
     
-    def eval:
+    # Evaluate results on test set
+    # Results saved to file for later inspection
+    def evaluate():
         eval_results = trainer.evaluate(dataset["test"])
         with open(f'../results/evaluation_{args.xp_name}.txt', 'w') as f:
             f.write('Accuracy: ')
@@ -206,6 +206,8 @@ def main():
             f.write(f'{eval_results["eval_f1"]}\n')
             f.write('Loss: ')
             f.write(f'{eval_results["eval_loss"]}\n')
+
+    evaluate()
 
 if __name__ == "__main__":
     main()
